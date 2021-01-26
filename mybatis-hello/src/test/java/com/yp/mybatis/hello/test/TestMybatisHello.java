85b2125a9c39f5c5cd4ef135a1e0d79b2d609329
painye <1850384745@qq.com>
2021-01-26 20:09:32 +0800

    将mybatis的运行过程进行了一些小的调整

--- mybatis-hello/src/test/java/com/yp/mybatis/hello/test/TestMybatisHello.java
+++ mybatis-hello/src/test/java/com/yp/mybatis/hello/test/TestMybatisHello.java
@@ -7,25 +7,46 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.Reader;
 
 public class TestMybatisHello {
-    @Test
-    public void testSave() throws Exception{
-        //读取配置文件 mybatis-configuration.xml
-        Reader reader = Resources.getResourceAsReader("mybatis-configuration.xml");
-        //SqlSessionFactory工厂
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        //sqlSession
-        SqlSession sqlSession=sessionFactory.openSession();
 
+    private SqlSession sqlSession;
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        init();
+    }
+
+    private static void init(){
+        try {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build( Resources.getResourceAsReader("mybatis-configuration.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    /**
+     * //读取配置文件 mybatis-configuration.xml
+     *         //Reader reader = Resources.getResourceAsReader("mybatis-configuration.xml");
+     *         //SqlSessionFactory工厂
+     *         //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+     */
+    public void testSave() throws Exception{
+        //sqlSession
+        sqlSession=sqlSessionFactory.openSession();
         //绑定接口
         IStudentMapper mapper = sqlSession.getMapper(IStudentMapper.class);//代理模式
+        mapper.save(new Student("李永乐", "201215001", "男", "SD", 23));
+    }
 
-        mapper.save(new Student("汤家凤", "201215000", "男", "SD", 23));
-
+    @After
+    public void finish(){
         sqlSession.commit();//事物的提交
         sqlSession.close();
     }
